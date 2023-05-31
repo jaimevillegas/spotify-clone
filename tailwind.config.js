61d54b63e9ba2fa264d84bfda5285902475f6c6b
Jaime Villegas <jaimevillegas296@gmail.com>
2023-05-31 14:27:24 -0500

    Environment Setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,7 @@ module.exports = {
   ],
   theme: {
     extend: {
+      // Delete this later
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
